a1ada2ae37ddc265a1992f30284942f9d53b56b0
Jamie Caprani <jamiecaprani@gmail.com>
2025-01-24 18:36:16 +0000

    chore: add basic typescript infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from './pages/HomePage'
 import { NFTPage } from './pages/NFTPage'
 import { AppStateProvider } from './appState'
 
-function App() {
+const App: React.FC= () => {
   return (
     <div className="App">
       <Router>
