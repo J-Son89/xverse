fb416a4a6f12050415fad9e309179e0618a14cb0
Jamie Caprani <jamiecaprani@gmail.com>
2025-01-25 02:54:07 +0000

    fix: unit tests to use query client provider

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -2,14 +2,19 @@ import {render, screen} from '@testing-library/react';
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {HomePage} from './index';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 test('renders HomePage screen', () => {
   render(
-    <BrowserRouter
-      future={{v7_relativeSplatPath: true, v7_startTransition: true}}
-    >
-      <HomePage />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter
+        future={{v7_relativeSplatPath: true, v7_startTransition: true}}
+      >
+        <HomePage />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
   const linkElement = screen.getByText(/Ordinal Inscription Lookup/i);
   expect(linkElement).toBeInTheDocument();
--- src/pages/NFTPage/NFTPage.test.tsx
+++ src/pages/NFTPage/NFTPage.test.tsx
@@ -2,14 +2,19 @@ import {render, screen} from '@testing-library/react';
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {NFTPage} from './index';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 test('renders NFTPage screen', () => {
   render(
-    <BrowserRouter
-      future={{v7_relativeSplatPath: true, v7_startTransition: true}}
-    >
-      <NFTPage />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter
+        future={{v7_relativeSplatPath: true, v7_startTransition: true}}
+      >
+        <NFTPage />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
   const linkElement = screen.getByText(/Details/i);
   expect(linkElement).toBeInTheDocument();
